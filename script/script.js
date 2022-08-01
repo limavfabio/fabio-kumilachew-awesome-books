3c2c2d3c0f4eec008e366e796349511e652f93c8
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-01 17:26:00 -0300

    Initial Implementation of add button

--- script/script.js
+++ script/script.js
@@ -17,17 +17,29 @@ const books = [
 // ------------------- Functions -------------------
 // A function to remove current object from the array
 function removeCard(index) {
-    
+  books.splice(index, 1);
 }
 
 // Create a function that will print the books cards
 // Create a variable that will hold the books container
 let booksContainer = document.getElementById('books-container');
 // Loop through the books array
-for (let i = 0; i < books.length; i++) {
+for (let index = 0; index < books.length; index++) {
   booksContainer.innerHTML += `<div class="book-card">
-    <div class="book-title">${books[i].title}</div>
-    <div class="book-author">${books[i].author}</div>
-    <button class="card-remove-button" onclick="removeCard(${books[i]})">Remove</button>
+    <div class="book-title">${books[index].title}</div>
+    <div class="book-author">${books[index].author}</div>
+    <button class="card-remove-button" onclick="removeCard(${index})">Remove</button>
   </div>`;
+}
+
+// Variables to hold the input values
+const titleInput = document.getElementById('title-input').value;
+const authorInput = document.getElementById('author-input').value;
+
+// A function to add a new book to the array
+function addBook() {
+  books.push({
+    title: titleInput,
+    author: authorInput,
+  });
 }
