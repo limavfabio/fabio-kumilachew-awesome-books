384e1fc949b5f3c48457463c0a10e177822e7132
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-01 16:21:28 -0300

    Add cards dinamically based on array of objects

--- script/script.js
+++ script/script.js
@@ -1,24 +1,36 @@
 // Create an array of objects for the books information
 const books = [
+  {
+    title: 'The Design of EveryDay Things',
+    author: 'Don Norman',
+  },
+  {
+    title: 'The Most Human Human',
+    author: 'Brian Christian',
+  },
     {
-        title: 'The Design of EveryDay Things',
-        author: 'Don Norman',
-    },
-    {
-        title: 'The Most Human Human',
-        author: 'Brian Christian',
+    title: 'A Brief History of Time',
+    author: 'Stephen Hawking',
     },
 ];
 
+// ------------------- Functions -------------------
+// A function to remove current object from the array
+function removeCard() {
+    
+}
+
+
+
+
 // Create a function that will print the books cards
-    // Create a variable that will hold the books container
-    let booksContainer = document.getElementById('books-container');
-    // Loop through the books array
-    for (let i = 0; i < books.length; i++) {
-        books.innerHTML +=  
-          <div class="book-card">
-            <div class="book-title">Book Title</div>
-            <div class="book-author">Book Author</div>
-          </div>
-        // Print the book card
-    }
+// Create a variable that will hold the books container
+let booksContainer = document.getElementById('books-container');
+// Loop through the books array
+for (let i = 0; i < books.length; i++) {
+  booksContainer.innerHTML += `<div class="book-card">
+    <div class="book-title">${books[i].title}</div>
+    <div class="book-author">${books[i].author}</div>
+    <button class="card-remove-button" onclick="removeCard">Remove</button>
+  </div>`;
+}
