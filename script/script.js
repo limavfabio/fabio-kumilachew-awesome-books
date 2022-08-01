35e2a362f8fdcd5585fe572de37ba75b0fc7b92f
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-01 15:36:30 -0300

    Add books array of objects

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// Create an array of objects for the books information
+const books = [
+    {
+        title: 'The Design of EveryDay Things',
+        author: 'Don Norman',
+    },
+    {
+        title: 'The Most Human Human',
+        author: 'Brian Christian',
+    },
+];
