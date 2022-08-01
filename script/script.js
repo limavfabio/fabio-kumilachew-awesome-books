dbca2761df956aa677e6a2f54e10fed770ab2f65
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-01 16:42:38 -0300

    Update card button to run removeCard() function

--- script/script.js
+++ script/script.js
@@ -8,20 +8,17 @@ const books = [
     title: 'The Most Human Human',
     author: 'Brian Christian',
   },
-    {
+  {
     title: 'A Brief History of Time',
     author: 'Stephen Hawking',
-    },
+  },
 ];
 
 // ------------------- Functions -------------------
 // A function to remove current object from the array
-function removeCard() {
+function removeCard(index) {
     
 }
-
-
-
 
 // Create a function that will print the books cards
 // Create a variable that will hold the books container
@@ -31,6 +28,6 @@ for (let i = 0; i < books.length; i++) {
   booksContainer.innerHTML += `<div class="book-card">
     <div class="book-title">${books[i].title}</div>
     <div class="book-author">${books[i].author}</div>
-    <button class="card-remove-button" onclick="removeCard">Remove</button>
+    <button class="card-remove-button" onclick="removeCard(${books[i]})">Remove</button>
   </div>`;
 }
