50c813f55e5351ffab692a730701a8431a8cf3c5
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-08-01 16:04:19 -0300

    Initial JS implementation

--- script/script.js
+++ script/script.js
@@ -9,3 +9,16 @@ const books = [
         author: 'Brian Christian',
     },
 ];
+
+// Create a function that will print the books cards
+    // Create a variable that will hold the books container
+    let booksContainer = document.getElementById('books-container');
+    // Loop through the books array
+    for (let i = 0; i < books.length; i++) {
+        books.innerHTML +=  
+          <div class="book-card">
+            <div class="book-title">Book Title</div>
+            <div class="book-author">Book Author</div>
+          </div>
+        // Print the book card
+    }
